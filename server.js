bde388c5a4eac2d19f5cfba25ff225bc621d2f01
Amit Singh Rana <ranaa73000@gmail.com>
2023-07-30 20:36:52 +0530

    Basic routing and file structuring for routes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import apiRoutes from "./routes/index.js";
+
 const app = express();
 dotenv.config();
 
 // Parse JSON data in body of a request
 app.use(express.json());
+
+// Attach API routes
+app.use("/api/v1", apiRoutes);
 
 // Server Setup
 const PORT = process.env.PORT || 3000;
