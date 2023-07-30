d388d67d736a762faf0b2e2700b2d4b780690032
Amit Singh Rana <ranaa73000@gmail.com>
2023-07-30 20:27:58 +0530

    Enabled request data parsing to JSON

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
+// Parse JSON data in body of a request
+app.use(express.json());
+
 // Server Setup
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
